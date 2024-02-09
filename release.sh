6517a8b38f8291f9486612348366ae7c9ea52c95
Rahul Sridhar <sridhar.rahul@gmail.com>
2024-02-09 02:07:00 +0000

    release script

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+VERSION="1.0.0"
+REPOSITORY="codeium-enterprise"
+
+docker build . -t "exafunction/$REPOSITORY/gotrue:$VERSION"
+
+# AWS
+../Exafunction/exa/deploy_images/ecr_login.sh
+
+docker tag "exafunction/$REPOSITORY/gotrue:$VERSION" "433863713167.dkr.ecr.us-east-2.amazonaws.com/exafunction/$REPOSITORY/gotrue:$VERSION"
+docker push "433863713167.dkr.ecr.us-east-2.amazonaws.com/exafunction/$REPOSITORY/gotrue:$VERSION"
+
+# GCP
+
+../Exafunction/exa/deploy_images/gcr_login.sh
+
+docker tag "exafunction/$REPOSITORY/gotrue:$VERSION" "us-docker.pkg.dev/exafunction/$REPOSITORY/gotrue:$VERSION"
+docker push "us-docker.pkg.dev/exafunction/$REPOSITORY/gotrue:$VERSION"
+
+# Azure
+
+../Exafunction/exa/deploy_images/acr_login.sh
+
+docker tag "exafunction/$REPOSITORY/gotrue:$VERSION" "exafunction.azurecr.io/exafunction/$REPOSITORY/gotrue:$VERSION"
+docker push "exafunction.azurecr.io/exafunction/$REPOSITORY/gotrue:$VERSION"
